23b47a48cca7397dc10e8e04c11280019261d3db
Salvador <A01029956@itesm.mx>
2022-04-18 11:34:15 -0500

    Modifying Hello JS file

--- Web/FRONT/js/hello_js.js
+++ Web/FRONT/js/hello_js.js
@@ -4,3 +4,18 @@ let var1 = 0
 const var2 = 1
 /*var var3 = 2*/
 /*Do not declare a variable as var var_name!*/
+
+var5 = [4, 5, 6, 7]
+
+for(let i = 0; i < var5.length; i++)
+    console.log(var5[i], var5[i] % 2)
+
+for(let n of var5)
+    console.log(n - 1)
+
+let count = 0
+while(count < 10)
+{
+    console.log(count)
+    count++
+}
