f59bcdc9478bb568d57a440048d8fbb8193006d7
Salvador <A01029956@itesm.mx>
2022-04-25 12:51:58 -0500

    Manipulating paths

--- TC2005B.500/Web/FRONT/js/eventos_dom.js
+++ TC2005B.500/Web/FRONT/js/eventos_dom.js
@@ -1,12 +1,44 @@
 document.addEventListener("mousemove", (event) =>
 {
-    const mousePos = document.getElementById('mousePosition')
+    const mousePosition = document.getElementById('mousePosition')
     mousePosition.innerHTML = `Mouse position: ${event.clientX} - ${event.clientY}`
-
 })
 
 document.addEventListener("submit", (event) =>
 {
+    event.preventDefault()
+    const fname = document.getElementsByName('fname')
+    const lname = document.getElementsByName('lname')
     const name = document.getElementById('name')
-    name 
+    name.innerHTML = `${fname[0].value} ${lname[0].value}`
 })
+
+function insertRow()
+{
+    const thisTable = document.getElementById('sampleTable')
+
+    let newRow = thisTable.insertRow()
+    let xRows = thisTable.rows.length
+    let xCols = thisTable.rows[0].cells.length
+
+    for(i = 0; i < xCols; i++)
+    {
+        let newCell = newRow.insertCell(i)
+        newCell.innerHTML = `Row ${xRows} Column ${i+1}`
+    }
+}
+
+function insertColumn()
+{
+    const thisTable = document.getElementById('sampleTable')
+
+    let xRows = thisTable.rows.length
+    let xCols = thisTable.rows[0].cells.length
+
+    for(i = 0; i < xRows; i++)
+    {
+        let row = thisTable.rows[i]
+        let newCol = row.insertCell()
+        newCol.innerHTML = `Row ${i+1} Col ${xCols+1}`
+    }
+}
