b3ef97cedc48b621ea9a55f20d0962ccd93af64d
Salvador <A01029956@itesm.mx>
2022-05-19 15:00:12 -0500

    removed node modules

--- TC2005B.500/Web/BACKEND/JS/modules3/hello.js
+++
@@ -1,6 +0,0 @@
-import {student} from './student.js'
-
-
-const student_1 = new student('Octavio','Navarro','octavio.navarro@tec.mx')
-
-student_1.logInfo()
--- TC2005B.500/Web/BACKEND/JS/users_test.js
+++
@@ -1,6 +0,0 @@
-import users from '../JS/users_module.js'
-
-const user1 = new users('Octavio','Navarro','octavio.navarro@tec.mx')
-
-
-user1.getInfo()
